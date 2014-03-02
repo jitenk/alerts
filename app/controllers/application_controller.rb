ff07eabf80e0832418328265a582031d3c40032d
Jiten K <jiten3@gmail.com>
2014-03-02 13:14:26 -0600

    fix bootstrap mistake + initate the nav bar with login UI enchancement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  layout :layout_by_resource
+
   def set_mailer_host
     ActionMailer::Base.default_url_options[:host] = request.host_with_port
+  end
+
+  # this method will not show application layout on login screen
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
   end
 
 end
