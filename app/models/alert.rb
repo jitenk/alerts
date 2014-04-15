d75caa1f6353db0bef15312a070ddc5111518848
Jiten Kothari <jiten.kothari@dps.texas.gov>
2014-04-15 16:36:02 -0500

    modified

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,6 +1,12 @@
 class Alert < ActiveRecord::Base
   has_many :victims
+  has_many :suspects
+
   accepts_nested_attributes_for :victims,
+                                :allow_destroy => true,
+                                :reject_if => lambda {|a| a[:full_name].blank?}
+
+  accepts_nested_attributes_for :suspects,
                                 :allow_destroy => true,
                                 :reject_if => lambda {|a| a[:full_name].blank?}
   belongs_to :alert_type
