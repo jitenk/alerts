7811205aa3623434d246c6e91cf04d507e3bb4f9
Jiten Kothari <jiten.kothari@dps.texas.gov>
2014-04-21 11:04:11 -0500

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410182515) do
+ActiveRecord::Schema.define(version: 20140421155507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20140410182515) do
     t.string   "age_unit"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "additional_info"
   end
 
   create_table "questions", force: true do |t|
