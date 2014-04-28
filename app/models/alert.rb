49d5d34e49ddebfb3df07525c5a81e95adbdbe26
Jiten Kothari <jiten.kothari@dps.texas.gov>
2014-04-28 16:22:57 -0500

    fix of nested_attribute not populating

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -2,14 +2,18 @@ class Alert < ActiveRecord::Base
 
   has_many :vehicles, :dependent => :destroy
   accepts_nested_attributes_for :vehicles
+                                #:allow_destroy => true,
+                                #:reject_if => lambda {|a| a[:full_name].blank?}
 
   has_many :victims, :dependent => :destroy
   accepts_nested_attributes_for :victims
+                                #:allow_destroy => true,
+                                #:reject_if => lambda {|a| a[:full_name].blank?}
 
   has_many :suspects, :dependent => :destroy
   accepts_nested_attributes_for :suspects
-
-
+                                #:allow_destroy => true,
+                                #:reject_if => lambda {|a| a[:full_name].blank?}
 
   belongs_to :alert_type
 
